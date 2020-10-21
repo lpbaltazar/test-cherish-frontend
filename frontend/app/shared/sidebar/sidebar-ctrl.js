a536df2f335c27a0026084e84e9e100a82388099
ririgi <lrpbaltazar@gmail.com>
2020-10-21 09:25:57 +0800

    hide aided if unfinished with all evaluation

--- frontend/app/shared/sidebar/sidebar-ctrl.js
+++ frontend/app/shared/sidebar/sidebar-ctrl.js
@@ -9,14 +9,20 @@
         '$scope', 
         '$state',
         '$stateParams',
-        '$cookies'
+        '$cookies',
+        'QueryService'
     ];
 
-    function SidebarCtrl ($scope, $state, $stateParams, $cookies) { 
+    function SidebarCtrl ($scope, $state, $stateParams, $cookies, QueryService) { 
 
-        var vm  = this;
+        var vm                      = this;
+        vm.pagination               = {};
+        vm.pagination.pagestate     = $stateParams.page || 1;
+        vm.pagination.size          = $stateParams.size || 10;
 
-        vm.user = {};
+        vm.user                     = {};
+        vm.totalImages             = 216;
+        vm.retrieveCXR              = retrieveCXR;
 
         init();
 
@@ -25,6 +31,7 @@
             vm.user = $cookies.getObject('user');
             vm[vm.user.role] = true;
             // accessControl(vm.user.roles);
+            retrieveCXR();
         
         }
 
@@ -35,6 +42,51 @@
             };
 
         }
+
+        function retrieveCXR (search) {
+
+            var params =  {
+                search: vm.search_text || '',
+                page: vm.pagination.pagestate,
+                size: vm.pagination.size
+            };
+            search = (vm.search_text == '') ? '':search;
+
+            if(vm.user.role === 'physician'){
+                var request = {
+                                method  : 'GET',
+                                body    : false,
+                                params  : params,
+                                hasFile : false,
+                                route   : { 'physician/cxr': search }
+                };
+            } else {
+                var request = {
+                                method  : 'GET',
+                                body    : false,
+                                params  : params,
+                                hasFile : false,
+                                route   : { 'radiologist/cxr': search }
+                };
+            };
+            
+            QueryService
+                .query(request)
+                .then( function (response) { 
+                    vm.items            = response.data.data.items; 
+                    vm.diagnosis        = vm.items[vm.items.length - 1];
+                    vm.items            = vm.items.splice(0, vm.items.length - 1);
+                    vm.count            = vm.items[vm.items.length - 1];
+                    vm.items            = vm.items.splice(0, vm.items.length - 1);
+                    vm.pagination.page  = $stateParams.page || 1;
+                    vm.pagination.total = response.data.data.total;
+                    vm.originalitems    = vm.items;
+                    console.log(vm.diagnosis)
+
+                }, function (error) {
+                    logger.error(error.data.errors[0].message);
+                });
+        };
     
     }
 
