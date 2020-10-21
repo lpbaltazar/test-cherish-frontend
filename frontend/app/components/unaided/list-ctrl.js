af7619214fdf062f4810f69de4a0920ccab355da
ririgi <lrpbaltazar@gmail.com>
2020-10-21 09:50:27 +0800

    fix filter

--- frontend/app/components/unaided/list-ctrl.js
+++ frontend/app/components/unaided/list-ctrl.js
@@ -32,6 +32,7 @@
         vm.retrieveCXR              = retrieveCXR;
         vm.addCount                 = addCount;
         vm.filterTable              = filterTable;
+        vm.combine                  = combine;
 
 
         init();
@@ -83,8 +84,9 @@
                     addCount(vm.items)
                     vm.originalitems    = vm.items;
                     console.log(vm.items)
-                    console.log(vm.diagnosis[0].length)
+                    console.log(vm.diagnosis[0])
                     console.log(vm.count)
+                    combine(vm.items, vm.diagnosis[0])
                 }, function (error) {
                     console.log(error);
                     logger.error(error.data.errors[0].message);
@@ -104,20 +106,41 @@
             $state.go('app.unaided', {page: page, size: $stateParams.size});
         };
 
+        function combine(items, diagnosis) {
+
+            for (var i=0; i<items.length; i++) {
+                var cxr_acc = items[i].cxr_accession_number;
+
+                for (var j=0; j<diagnosis.length; j++) {
+                    var diagnosis_acc = diagnosis[j].diagnosis_accession_number;
+                    
+                    if (cxr_acc === diagnosis_acc) {
+                        items[i]["finished"] = true;
+                        diagnosis = diagnosis.filter(it => it.diagnosis_accession_number !== cxr_acc)
+                    }
+                    // } else {
+                    //     items[i]["finished"] = false;
+                    // }
+                }
+            }
+            vm.items = items;
+            console.log(items);
+        };
+
         function filterTable(status) {
             var originalitems = vm.originalitems;
 
             if (status === "All") {
                 vm.items = originalitems;
             } else if (status === "Done") {
-                let items = vm.originalitems.filter(it => it.diagnosis_diagnosis_unaided_finalized === 1);
+                let items = vm.originalitems.filter(it => it.finished);
                 vm.items = items;
             } else {
-                let items = vm.originalitems.filter(it => it.diagnosis_diagnosis_unaided_finalized === null);
+                let items = vm.originalitems.filter(it => !it.finished);
                 vm.items = items;
             }
 
-            x => x !== null
+            // x => x !== null
             // console.log(vm.items)
         };
 
